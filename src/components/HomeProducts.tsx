a4d2c1e98caca51f974f6f845c98ad196ce864a0
Juan Martinez <juanmartinez@Juans-iMac.local>
2024-01-15 14:50:36 -0500

    fixed a bug

--- src/components/HomeProducts.tsx
+++ src/components/HomeProducts.tsx
@@ -1,12 +1,10 @@
-import { Link } from "react-router-dom"
 type HomeProductProps = {
-    link: string;
     src: string;
     price:string;
     productName:string;
 }
 
-export default function HomeProducts ({src,productName,price,link}:HomeProductProps){
+export default function HomeProducts ({src,productName,price}:HomeProductProps){
 const backgroundImage:React.CSSProperties = {
 backgroundImage:`url(${src})`
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,25 +8,24 @@ const Home = () => {
           src={trees}
           price="10"
           productName="Shirts"
-          link="/Shirts"
+      
         />
         <HomeProducts
           src={trees}
           price="30-50"
           productName="Arts and Crafts"
-          link="/Shirts"
+      
         />{" "}
         <HomeProducts
           src={trees}
           price="20-30"
           productName="Signs"
-          link="/signs"
         />{" "}
         <HomeProducts
           src={trees}
           price="10-40"
           productName="Special Occasions"
-          link="/Shirts"
+      
         />
       </div>
     </>
